4f31b21a6a1f878deafbda37386720a144ff53b2
Rana Banerjee <rana1224@gmail.com>
2024-10-23 18:29:49 +0530

    updated mail message

--- monitor_trade.py
+++ monitor_trade.py
@@ -460,11 +460,13 @@ def monitor_and_execute_trades(target_profit, stop_loss, lots):
             api.logout()
         else:
             # Exit and create adjustment Legs:
+            email_subject = f"<<< ADJUSTMENT MADE | Revised DELTA: {new_delta} >>>"
             exit_positions(exit_order_df)
             # Place leg and hedge orders
             # Place leg and hedge orders
             place_order("B", H_tsym, lots*lot_size, remarks="Adjustment Hedge order")
             place_order("S", L_tsym, lots*lot_size, remarks="Adjustment Sell order")
+            logger.info(f"REVISED DELTA: {new_delta}")
             api.logout()
     
 
