58e061fe44dc19f7bcca25c3eb838b0912adf483
Rana Banerjee <rana1224@gmail.com>
2024-10-25 13:19:42 +0530

    update

--- monitor_trade.py
+++ monitor_trade.py
@@ -191,7 +191,7 @@ def trailing_profit_exit(csv_file):
             save_state(trailing_profit_threshold, in_trailing_mode, csv_file, df)
             return False
     else:
-        save_state(trailing_profit_threshold, in_trailing_mode, csv_file, df)
+        save_state(target_profit, False, csv_file, df)
         return False
 
 # Step 1: Preprocess data and extract necessary information
