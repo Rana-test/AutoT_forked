30b34ba8f54e498dd4a4e6e5f21c6f1e23ac2941
Rana Banerjee <rana1224@gmail.com>
2024-10-28 10:26:48 +0530

    Update monitor_trade.py

--- monitor_trade.py
+++ monitor_trade.py
@@ -325,7 +325,6 @@ def get_Option_Chain(type):
     for i in df.index:
         strikeInfo = df.loc[i]
         res=api.get_quotes(exchange="NFO", token=str(strikeInfo.Token))
-        print(res)
         res={'oi':res['oi'], 'tsym':res['tsym'], 'lp':float(res['lp']), 'lotSize':strikeInfo.LotSize, 'token':res['token'], 'StrikePrice':int(float(res['strprc']))}
         OList.append(res)
     Ostrikedf = pd.DataFrame(OList)
