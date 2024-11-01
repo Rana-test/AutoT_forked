6c14e8475de9e4bc3f3ca41d672f8d4ebf0e640b
Rana Banerjee <rana1224@gmail.com>
2024-11-01 18:36:03 +0530

    Update monitor_trade.py

--- monitor_trade.py
+++ monitor_trade.py
@@ -792,7 +792,7 @@ if __name__=="__main__":
     # past_930 = datetime.strptime("04:00:00", "%H:%M:%S").time()
     # eod_10 = datetime.strptime("10:02:00", "%H:%M:%S").time()
     past_930 = datetime.strptime("12:30:00", "%H:%M:%S").time()
-    eod_10 = datetime.strptime("13:02:00", "%H:%M:%S").time()
+    eod_10 = datetime.strptime("13:32:00", "%H:%M:%S").time()
     if not os.path.exists('logs'):
         os.makedirs('logs')
     # Login to Shoonya app
