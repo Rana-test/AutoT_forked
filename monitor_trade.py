1fd66a20633b0c42f5953079979bb0ca289981e8
Rana Banerjee <rana1224@gmail.com>
2024-11-01 17:58:46 +0530

    muhurat update

--- monitor_trade.py
+++ monitor_trade.py
@@ -789,8 +789,10 @@ def monitor_loop():
 
 # Call the main function periodically to monitor and execute trades
 if __name__=="__main__":
-    past_930 = datetime.strptime("04:00:00", "%H:%M:%S").time()
-    eod_10 = datetime.strptime("10:02:00", "%H:%M:%S").time()
+    # past_930 = datetime.strptime("04:00:00", "%H:%M:%S").time()
+    # eod_10 = datetime.strptime("10:02:00", "%H:%M:%S").time()
+    past_930 = datetime.strptime("12:30:00", "%H:%M:%S").time()
+    eod_10 = datetime.strptime("13:02:00", "%H:%M:%S").time()
     if not os.path.exists('logs'):
         os.makedirs('logs')
     # Login to Shoonya app
