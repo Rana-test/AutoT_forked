a371ead64d7318d596d1c87a7a7ed193069fffd7
Rana Banerjee <rana1224@gmail.com>
2024-12-16 18:48:12 +0530

    update breakeven

--- helpers/helper.py
+++ helpers/helper.py
@@ -1211,9 +1211,8 @@ def upstox_calculate_breakevens(df, global_vars):
     # Calculate breakevens
     # qty = global_vars.get('lot_size')*global_vars.get('lots')
     df['total_credit'] = df['netupldprc'].astype(float).astype(int)
-    # adj = global_vars.get('Past_M2M')/df['qty'].abs().mean()
     df['qty'] = pd.to_numeric(df['qty'], errors='coerce')
-    adj = abs(float(global_vars.get('Past_M2M')) / df['qty'].mean())
+    adj = global_vars.get('Past_M2M')/df['qty'].abs().mean()
     net_credit = round(float(df['total_credit'].sum()),2)
     higher_be = float(df[(df['ord_type']=="P")&(df['buy_sell']=="S")]['tsym'].iloc[0][-7:-2])+net_credit -adj
     lower_be = float(df[(df['ord_type']=="C")&(df['buy_sell']=="S")]['tsym'].iloc[0][-7:-2])-net_credit +adj
@@ -1221,13 +1220,11 @@ def upstox_calculate_breakevens(df, global_vars):
 
 def calculate_breakevens(df, global_vars):
     # Calculate breakevens
-    # qty = global_vars.get('lot_size')*global_vars.get('lots')
-    # df['total_credit'] = df['netupldprc'].astype(float).astype(int)
-    # df['qty'] = pd.to_numeric(df['qty'], errors='coerce')
-    # # adj = global_vars.get('Past_M2M')/df['qty'].abs().mean()
-    # adj = abs(float(global_vars.get('Past_M2M')) / df['qty'].mean())
-    # net_credit = round(float(df['total_credit'].sum()),2)
-    # higher_be = float(df[(df['ord_type']=="P")&(df['buy_sell']=="S")]['tsym'].iloc[0][-5:])+net_credit #-adj
-    # lower_be = float(df[(df['ord_type']=="C")&(df['buy_sell']=="S")]['tsym'].iloc[0][-5:])-net_credit #+adj
-    # return round(lower_be,0), round(higher_be,0)
-    return 0, 0 
+    qty = global_vars.get('lot_size')*global_vars.get('lots')
+    df['total_credit'] = df['netupldprc'].astype(float).astype(int)
+    df['qty'] = pd.to_numeric(df['qty'], errors='coerce')
+    adj = global_vars.get('Past_M2M')/df['qty'].abs().mean()
+    net_credit = round(float(df['total_credit'].sum()),2)
+    higher_be = float(df[(df['ord_type']=="P")&(df['buy_sell']=="S")]['tsym'].iloc[0][-5:])+net_credit #-adj
+    lower_be = float(df[(df['ord_type']=="C")&(df['buy_sell']=="S")]['tsym'].iloc[0][-5:])-net_credit #+adj
+    return round(lower_be,0), round(higher_be,0)
